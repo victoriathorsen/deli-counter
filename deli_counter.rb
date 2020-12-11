ce066133bda10239b55f44d3878510715491524c
Victoria Thorsen <victoriathorsenn@gmail.com>
2020-12-11 02:08:20 -0800

    commit tree dcf4723a1b4af2a70d57801e5787a307b377cb3f

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def line(deli)
+    if deli.length == 0
+        puts "The line is currently empty."
+    else 
+        line= "The line is currently:"
+        deli.each_with_index do |name, i| 
+        line += " #{i+1}. #{name}"
+        end
+    puts line
+    return line
+    end
+end
+
+def take_a_number (deli, person)
+    deli.push(person)
+    puts "Welcome, #{person}. You are number #{deli.length} in line."
+
+end
+
+def now_serving (deli)
+    if deli.length > 0
+        puts "Currently serving #{deli[0]}."
+        deli.shift
+    else
+        puts "There is nobody waiting to be served!" 
+    end
+end
+
